77be93b9beb6775118a053192bdfa51b9a3f5c83
s00181848 <36032381+s00181848@users.noreply.github.com>
2019-02-26 19:02:32 +0000

    Adding Payment Class Properties

--- StudentPlanner/Models/PaymentTask.cs
+++ StudentPlanner/Models/PaymentTask.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace StudentPlanner.Models {
     class PaymentTask {
+        //properties
+        public decimal Amount { get; set; }
 
-    }
+    }//end of the payment class
 }
