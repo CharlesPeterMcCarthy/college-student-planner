33a61e874d25022c593a2b197b8c52056c002e1d
s00181848 <36032381+s00181848@users.noreply.github.com>
2019-02-26 18:45:22 +0000

    Adding Day Class Properties

--- StudentPlanner/Models/Day.cs
+++ StudentPlanner/Models/Day.cs
@@ -7,5 +7,10 @@ using System.Threading.Tasks;
 namespace StudentPlanner.Models {
     class Day {
 
-    }
+        //properties
+        public string DayOfWeek { get; set; }
+        public DateTime Date { get; set; }
+        public Task[] Tasks { get; set; }
+
+    }//end of the Day Class
 }
