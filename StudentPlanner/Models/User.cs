3018f42e3a12980532129d848cbab056ac0447c9
s00181848 <36032381+s00181848@users.noreply.github.com>
2019-02-26 18:54:52 +0000

    Adding User Class Properties

--- StudentPlanner/Models/User.cs
+++ StudentPlanner/Models/User.cs
@@ -7,5 +7,11 @@ using System.Threading.Tasks;
 namespace StudentPlanner.Models {
     class User {
 
-    }
+        //properties
+        public string StudentID { get; set; }
+        public string Name { get; set; }
+        public DateTime DOB { get; set; }
+        public Planner Planner { get; set; }
+
+    }//end of the user class
 }
