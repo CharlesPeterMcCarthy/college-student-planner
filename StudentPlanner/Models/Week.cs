014e79f4ecd446eb2a82743751ec599459a2757c
s00181848 <36032381+s00181848@users.noreply.github.com>
2019-02-26 18:47:47 +0000

    Adding Week Class Properties

--- StudentPlanner/Models/Week.cs
+++ StudentPlanner/Models/Week.cs
@@ -7,5 +7,11 @@ using System.Threading.Tasks;
 namespace StudentPlanner.Models {
     class Week {
 
-    }
+        //properties
+        public int WeekNumber { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Day[] Days { get; set; }
+
+    }//end of the Week Class
 }
