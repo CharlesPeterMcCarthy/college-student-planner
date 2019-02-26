8e9c1fe924559ff8c63a496d567b9057221a9463
s00181848 <36032381+s00181848@users.noreply.github.com>
2019-02-26 18:58:13 +0000

    Adding Assignment Class Properties

--- StudentPlanner/Models/AssignmentTask.cs
+++ StudentPlanner/Models/AssignmentTask.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace StudentPlanner.Models {
     class AssignmentTask {
+        //properties
+        public string Subject { get; set; }
+        public int PercentageWorth { get; set; }
 
-    }
+    }//end of Assignment Class
 }
