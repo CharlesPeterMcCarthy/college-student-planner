7ec7a57b4519adcc49de8f69256d3e0cb5929720
s00181848 <36032381+s00181848@users.noreply.github.com>
2019-02-26 18:59:15 +0000

    Adding Event Class Properties

--- StudentPlanner/Models/EventTask.cs
+++ StudentPlanner/Models/EventTask.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace StudentPlanner.Models {
     class EventTask {
+        //properties
+        public string Location { get; set; }
 
-    }
+    }//end of event class
 }
