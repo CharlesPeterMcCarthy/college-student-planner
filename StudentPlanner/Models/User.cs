2235d0dfd2679f4327f65c51fbf274bf0618ab88
s00181848 <36032381+s00181848@users.noreply.github.com>
2019-02-27 13:50:10 +0000

    User Class Method GetAge

--- StudentPlanner/Models/User.cs
+++ StudentPlanner/Models/User.cs
@@ -13,5 +13,35 @@ namespace StudentPlanner.Models {
         public DateTime DOB { get; set; }
         public Planner Planner { get; set; }
 
+        //methods
+        public int GetAge(DateTime DOB)
+        {
+            int age=0;
+            DateTime Now = DateTime.Now;
+            age = Now.Year - DOB.Year;
+            if (DOB.Month > Now.Month)
+            {
+                age = Now.Year - DOB.Year;
+                age -= 1;
+                return age;
+            }
+            else
+            {
+                if (DOB.Day < Now.Day)
+                {
+                    age = Now.Year - DOB.Year;
+                    return age;
+                }
+                else
+                {
+                    age = Now.Year - DOB.Year;
+                    age -= 1;
+                    return age;
+                }
+
+            }
+
+        }
+
     }//end of the user class
 }
