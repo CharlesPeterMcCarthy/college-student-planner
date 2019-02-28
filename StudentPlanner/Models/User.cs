c580b8e8786e84c7ad92fa302ab490185befa7cc
Charles McCarthy <charles@studybundles.com>
2019-02-28 17:32:11 -0500

    Set User Age By DOB

--- StudentPlanner/Models/User.cs
+++ StudentPlanner/Models/User.cs
@@ -11,6 +11,7 @@ namespace StudentPlanner.Models {
         public string StudentID { get; private set; }
         public string Name { get; private set; }
         public DateTime DOB { get; private set; }
+        public int Age { get; private set; }
         public Planner Planner { get; private set; }
 
         public User(string studentID, string name, DateTime dob, Planner planner) : this(studentID, name, dob) {
@@ -21,6 +22,15 @@ namespace StudentPlanner.Models {
             StudentID = studentID;
             Name = name;
             DOB = dob;
+
+            SetAge();
+        }
+
+        private void SetAge() {
+            DateTime today = DateTime.Today;
+            Age = today.Year - DOB.Year;
+
+            if (DOB > today.AddYears(-Age)) Age--;
         }
 
     }//end of the user class
