beeccc88a41ee57f33476a3188728cf874156d24
Charles McCarthy <charles@studybundles.com>
2019-03-03 13:23:54 -0500

    Planner & Add Task Blank Pages

---
+++ StudentPlanner/MyPlanner.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StudentPlanner {
+    /// <summary>
+    /// Interaction logic for MyPlanner.xaml
+    /// </summary>
+    public partial class MyPlanner : Page {
+        public MyPlanner() {
+            InitializeComponent();
+        }
+    }
+}
