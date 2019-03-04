16c67cd477911a66eb2cd6b679f9be9b9ba40817
Charles McCarthy <charles@studybundles.com>
2019-03-04 17:56:39 +0000

    Display Daily Tasks

--- StudentPlanner/MyPlanner.xaml.cs
+++ StudentPlanner/MyPlanner.xaml.cs
@@ -28,6 +28,14 @@ namespace StudentPlanner {
 
         public MyPlanner(Planner p): this() {
             Planner = p;
+
+            monList.ItemsSource = p.Weeks[0].Days.Find(d => d.DayOfWeek == DayOfWeek.Monday).Tasks;
+            tuesList.ItemsSource = p.Weeks[0].Days.Find(d => d.DayOfWeek == DayOfWeek.Tuesday).Tasks;
+            wedList.ItemsSource = p.Weeks[0].Days.Find(d => d.DayOfWeek == DayOfWeek.Wednesday).Tasks;
+            thurList.ItemsSource = p.Weeks[0].Days.Find(d => d.DayOfWeek == DayOfWeek.Thursday).Tasks;
+            friList.ItemsSource = p.Weeks[0].Days.Find(d => d.DayOfWeek == DayOfWeek.Friday).Tasks;
+            satList.ItemsSource = p.Weeks[0].Days.Find(d => d.DayOfWeek == DayOfWeek.Saturday).Tasks;
+            sunList.ItemsSource = p.Weeks[0].Days.Find(d => d.DayOfWeek == DayOfWeek.Sunday).Tasks;
         }
     }
 }
